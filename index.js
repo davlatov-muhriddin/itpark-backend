4f78fcba1734f4f19f77ec2750fcda8f045e0750
davlatov-muhriddin <muhriddind95@gmail.com>
2023-12-27 09:02:16 +0500

    added cors

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import connectToDatabase from "./databse.js";
 import courseRouter from "./routes/courses.js";
 import usersRouter from "./routes/users.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/courses", courseRouter);
 app.use("/api/users", usersRouter);
