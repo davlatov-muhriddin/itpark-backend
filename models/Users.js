1a824bf7273ed81b84044953fe74a7880a22bece
davlatov-muhriddin <muhriddind95@gmail.com>
2023-12-29 10:33:57 +0500

    set timestamps

--- models/Users.js
+++ models/Users.js
@@ -1,11 +1,14 @@
 import { model, Schema } from "mongoose";
 
-const UserSchema = new Schema({
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  phone: { type: String, required: true },
-  course: { type: String, required: true },
-});
+const UserSchema = new Schema(
+  {
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    phone: { type: String, required: true },
+    course: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Users = model("users", UserSchema);
 export default Users;
